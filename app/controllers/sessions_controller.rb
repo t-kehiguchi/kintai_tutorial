d716891bb4deed96e3fe09a13367f31c38b4667a
t-kehiguchi <ec2-user@ip-172-31-45-50.ap-northeast-1.compute.internal>
2019-09-29 06:33:39 +0000

    basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+       # ユーザーログイン後にユーザー情報のページにリダイレクトする
+       log_in(user)
+      # log_in user
+       redirect_to user
+    else
+      # エラーメッセージ用のflashを入れる
+      flash.now[:danger] = 'メールアドレスとパスワードの情報が一致しませんでした。'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
